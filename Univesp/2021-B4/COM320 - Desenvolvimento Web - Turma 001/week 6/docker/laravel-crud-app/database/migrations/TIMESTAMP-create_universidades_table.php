10846b56e1dbdf7fcdb35c0a8213a0503eedea2f
Rafael Ito <ito.rafael@gmail.com>
2021-12-06 17:04:08 -0300

    [univesp] COM320: new Dockerfile v1.1 created (app from scratch)

--- Univesp/2021-B4/COM320 - Desenvolvimento Web - Turma 001/week 6/docker/laravel-crud-app/database/migrations/TIMESTAMP-create_universidades_table.php
+++ Univesp/2021-B4/COM320 - Desenvolvimento Web - Turma 001/week 6/docker/laravel-crud-app/database/migrations/TIMESTAMP-create_universidades_table.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Models\Pais;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateUniversidadesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +14,12 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('universidades', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+    	    $table->string('nome');
+	        $table->string('descricao', 600);
+	        $table->date('dt_fundacao');
+    	    $table->foreignIdFor(Pais::class); // pais_id
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('universidades');
     }
 }
